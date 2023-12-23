382464948e5e361ec9ba6d3c7fa9767d17bd6a27
yuta_ozawa <a2307yo@aiit.ac.jp>
2023-12-23 20:47:51 +0900

    add:fastapiのボイラープレートを作成

---
+++ sql_app/schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+# from pydantic_settings import BaseSettings
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        # orm_mode = True
+        from_attributes = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
+    class Config:
+        # orm_mode = True
+        from_attributes = True
