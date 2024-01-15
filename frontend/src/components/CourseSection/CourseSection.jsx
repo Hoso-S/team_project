44cb2a2d5c7c3f5d8a33ba249c72ae8aec3f5d1b
yamashi-ta-keshi <a2335ty@aiit.ac.jp>
2024-01-16 00:45:13 +0900

    科目画面の検索パート修正

--- frontend/src/components/CourseSection/CourseSection.jsx
+++ frontend/src/components/CourseSection/CourseSection.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
 import { courseSection } from '../../assets/sampleData.js'
 import { Box } from '@mui/material';
+// import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 // const rows = [
 //     { id: 1, col1: 'Hello', col2: 'World' },
@@ -24,9 +36,23 @@ const columns = [
 ];
 
 export default function Home() {
+  const onClickSearch = () => {
+    console.log('search');
+  }
+
+  const [semester, setSemester] = useState('');
+  const handleSemesterChange = (event) => {
+    setSemester(event.target.value);
+  };
+
+  const [year, setYear] = useState(null)
+  const handleYearChange = (newValue) => {
+    setYear(newValue)
+  }
+
   return (
     <>
-      <Box>
+      <Box sx={{ mb: 3 }}>
         <Accordion>
           <AccordionSummary
             expandIcon={<ArrowDropDownIcon />}
@@ -36,10 +62,52 @@ export default function Home() {
             <Typography>検索</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-              malesuada lacus ex, sit amet blandit leo lobortis eget.
-            </Typography>
+            <Box sx={{ display: 'flex' }}>
+              <Grid container>
+                <Grid item xs={12} sm={12}>
+                  <FormControl fullWidth>
+                    <TextField id="course-name" label="コース名" variant="standard" />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={12} sx={{ mt: 1 }}>
+                  {/* <TextField id="year" label="年度" variant="standard" /> */}
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="年度"
+                      value={year}
+                      onChange={handleYearChange}
+                      views={['year']}
+                      slot={{ textField: { variant: 'outlined' } }}
+                    />
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12} sm={12} sx={{ mt: 1 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">セメスター</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={semester}
+                      onChange={(handleSemesterChange)}
+                    >
+                      <MenuItem value={1}>Spring</MenuItem>
+                      <MenuItem value={2}>Summer</MenuItem>
+                      <MenuItem value={3}>Fall</MenuItem>
+                      <MenuItem value={4}>Winter</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <Button
+                    variant="contained"
+                    onClick={onClickSearch}
+                    sx={{ mt: 3, ml: 1 }}
+                  >
+                    検索
+                  </Button>
+                </Box>
+              </Grid>
+            </Box>
           </AccordionDetails>
         </Accordion>
       </Box>
